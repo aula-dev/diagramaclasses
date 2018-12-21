79b4dac5f40047c22d87633ec0372c7ade0c6829
pedroGuimaSena <pedro.gui.sena@gmail.com>
2018-12-21 15:45:38 -0200

    Classe Estado implementada

--- src/diagramaclasse/Estado.java
+++ src/diagramaclasse/Estado.java
@@ -5,10 +5,76 @@
  */
 package diagramaclasse;
 
+import java.util.Objects;
+
 /**
  *
  * @author Mariana
  */
 public class Estado {
+  private Integer id;
+  private String nome;
+
+  /**Método Construtor:
+     * @param id
+     * @param nome */
+    public Estado(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+   /** Método Construtor Vazio */
+    public Estado() {
+    }
+
     
+    /** Getters e Setters:
+     * @return  */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    /** Equals e Hash
+     * @return  */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estado other = (Estado) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+    
+  
 }
