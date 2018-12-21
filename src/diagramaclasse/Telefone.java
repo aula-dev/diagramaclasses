81fd067f8b42daadd992f800b6cfffbe32e2b9ab
pedroGuimaSena <pedro.gui.sena@gmail.com>
2018-12-21 15:48:47 -0200

    Classe Telefone implementada

--- src/diagramaclasse/Telefone.java
+++ src/diagramaclasse/Telefone.java
@@ -5,10 +5,63 @@
  */
 package diagramaclasse;
 
+import java.util.Objects;
+
 /**
  *
  * @author Mariana
  */
 public class Telefone {
+    private String numero;
+
+   /** Método Construtor:
+     * @param numero */
+    public Telefone(String numero) {
+        this.numero = numero;
+    }
+
+    
+    /** Método Construtor Vazio */
+    public Telefone() {
+    }
+
+    /** Get e Set :
+     * @return  */
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    
+    /** Equals e Hash:
+     * @return  */
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.numero);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Telefone other = (Telefone) obj;
+        return Objects.equals(this.numero, other.numero);
+    }
+    
+    
+    
+    
     
 }
